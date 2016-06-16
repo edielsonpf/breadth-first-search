fcd67f9cb13c7ea87a37f5bfe0721b15aaeaa427
Edielson P. Frigieri <edielsonpf@gmail.com>
2016-06-16 09:35:12 -0500

    adding problem class to make easy porting for other problems

--- python/SearchAlgorithms/bfs.py
+++ python/SearchAlgorithms/bfs.py
@@ -7,35 +7,59 @@ from queue import Queue
         
 class breadth_first_search(object):
     '''
-    classdocs
+    This class implements the breadth first search algorithm
     '''
 
-    def __init__(self, graph):
+    def __init__(self, problem):
         '''
         Constructor
+        Any object instance must receive an ``problem`` parameter that is responsible
+        for controlling the problem evaluation and next possible solutions. This parameter
+        have two required functions:
+            
+            ExpandSolution(current): function that returns all possible solutions from 
+            ``current`` state
+            TestObjective(current,target): function that evaluates if ``current`` state
+            corresponds to the ``target`` state 
+         
         '''
-        self.g = graph
-    
+        self.problem = problem
+        
     def search(self,start,target):
+        #start a empty queue
         frontier = Queue()
+        #insert ``start`` state in the queue
         frontier.put(start)
+        
+        #initialize control variables
         path = []
         come_from={}
         come_from[start]=None
         solution = False
         
+        #repeat while there are not visited candidate solutions
         while not frontier.empty():
+            #take the first candidate solution
             current = frontier.get()
+            #add to the visited states
             path.append(current)
             
-            if current == target:
+            #evaluate is this is the objective
+            if self.problem.ObjectiveTest(current,target) == True:
+                #if true, finish serach
                 solution = True
                 break
             
+            #else
             print("Visiting %r" % current)
-            for next_item in self.g.neighbors(current):
+            #expand new candidate solutions from current 
+            new_solutions = self.problem.ExpandSolution(current)
+            #run over all expanded solutions 
+            for next_item in new_solutions:
+                #check if each expanded solution was already visited
                 if next_item not in come_from:
+                    #if not, add to the queue for evaluation
                     frontier.put(next_item)
                     come_from[next_item] = current
                     
-        return come_from,path,solution        
+        return come_from,path,solution            
--- python/main.py
+++ python/main.py
@@ -1,10 +1,38 @@
 from SearchAlgorithms.bfs import breadth_first_search
 from graph import SimpleGraph
 
+class FindPath(object): 
+    '''
+    classdocs
+    '''
+
+    def __init__(self, graph):
+        '''
+        Constructor
+        '''
+        self.problem = graph
+        
+    def ObjectiveTest(self,current,target): 
+        """Return ``True`` if ``current`` state corresponds to the ``target`` state 
+        """ 
+        solution = False 
+        if current == target:
+            solution = True
+        return  solution
+ 
+    def ExpandSolution(self,current): 
+        """Returns all possible states from ``current`` 
+        """ 
+        return  self.problem.neighbors(current)
+    
+
 
 if __name__ == '__main__':
     
+    #Creating a simple graph object
     example_graph = SimpleGraph()
+    
+    #Adding possible edges for this graph
     example_graph.edges = {
         'Pouso Alegre': ['Santa Rita','Varginha','Congonhal','Cachoeira de Minas'],
         'Santa Rita': ['Pouso Alegre', 'Itajuba', 'Cachoeira de Minas'],
@@ -15,17 +43,43 @@ if __name__ == '__main__':
         'Belo Horizonte':['Varginha']
     }
     
-    SerachObject = breadth_first_search(example_graph)    
-    came_from,path,solution = SerachObject.search('Pouso Alegre','Belo Horizonte')
-    print(came_from)
+    #Creating an problem object based on FindPath class
+    Problema = FindPath(example_graph)
+    
+    #Creating an object for breadth first search algorithm for ``FindPath`` problem
+    SerachObject = breadth_first_search(Problema)    
+    
+    
+    #Finding solution
+    print('Searching Belo Horizonte starting from Pouso Alegre...')
+    start = 'Pouso Alegre'
+    target = 'Belo Horizonte'
+    came_from,path,solution = SerachObject.search(start,target)
+    print('Done!\n')
     if solution == True:
-        print(path)
+        print('Path found!')
+        string=(start)
+        for city in path:
+            if city != start:
+                string=(string+'->'+city)
+        print(string)
+    else:
+        print('Path not found!')        
         
-    print('Searching Itajuba starting from Belo Horizonte')
+    print('\nSearching Itajuba starting from Belo Horizonte...')
+    start = 'Belo Horizonte'
+    target = 'Itajuba'
     came_from,path,solution = SerachObject.search('Belo Horizonte','Itajuba')
-    print(came_from)
+    print('Done!\n')
     if solution == True:
-        print(path)
+        print('Path found!')
+        string=(start)
+        for city in path:
+            if city != start:
+                string=(string+'->'+city)
+        print(string)
+    else:
+        print('Path not found!')   
     
         
     
